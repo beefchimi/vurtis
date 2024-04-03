876ba9c6307da9e86f2f1dff4f593401f4ff9ba3
beefchimi <curtdulmage@gmail.com>
2024-04-03 16:44:21 -0400

    :sparkles: [useVurttle] Add a debounce option

--- src/useVurttle.ts
+++ src/useVurttle.ts
@@ -7,28 +7,41 @@ import {useMounted, useTimeout} from './hooks';
 // the `pending` state will become `true` and the timer will
 // begin counting down before returning to `false`. This is
 // useful for when you need to perform a side-effect to
-// virtual container/item changes. A common use-case for
-// this throttling layout animations during resize operations.
-// This may be necessary to avoid very aggresive re-renders.
+// virtual container/item changes. A common use-case is
+// throttling layout animations during resize operations.
+// If you need an even more aggressive deferral of side-effects,
+// you can pass the `debounce` argument which should further
+// reduce the number of `pending` changes.
 
 export const VURTTLE_DURATION = 200;
 
-export function useVurttle(vurtValue = 0) {
+export function useVurttle(vurtValue = 0, debounce = false) {
   const isMounted = useMounted();
+
   const [pending, setPending] = useState(false);
+  const [duration, setDuration] = useState(VURTTLE_DURATION);
 
   const handleReset = useCallback(() => {
     setPending(false);
   }, []);
 
   useEffect(() => {
-    if (isMounted() && !pending && vurtValue) {
-      setPending(true);
+    if (!isMounted() || !vurtValue) return;
+
+    setPending(true);
+
+    // Toggling the `duration` back-and-forth between a
+    // different value allows us to reset the `useTimeout()`,
+    // turning this hook into a "debounce" vs a "throttle".
+    if (debounce) {
+      setDuration((current) =>
+        current === VURTTLE_DURATION ? VURTTLE_DURATION + 1 : VURTTLE_DURATION,
+      );
     }
-  }, [isMounted, vurtValue]);
+  }, [isMounted, vurtValue, debounce]);
 
   useTimeout(handleReset, {
-    duration: VURTTLE_DURATION,
+    duration,
     disabled: !isMounted() || !pending,
   });
 
