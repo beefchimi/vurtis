f7eab40483ce210249dd11065624a3f2702af8b0
beefchimi <curtdulmage@gmail.com>
2024-05-30 15:04:38 -0400

    :hammer_and_wrench: [Eslint] Migrate config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,29 @@
-{
-  "env": {
-    "browser": true,
-    "es2022": true,
+import globals from 'globals';
+import configLove from 'eslint-config-love';
+// Includes both `config` and `plugin`.
+import pluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    ignores: ['coverage/**', 'dist/**'],
   },
-  "extends": ["love", "plugin:prettier/recommended"],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": "./tsconfig.json",
+  configLove,
+  pluginPrettier,
+  {
+    name: 'custom-rules',
+    files: ['**/*.ts', '**/*.js', '**/*.mjs'],
+    languageOptions: {
+      ...configLove.languageOptions,
+      globals: {
+        ...globals.browser,
+      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+    },
   },
-  "rules": {
-    "no-console": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-  },
-}
+];
